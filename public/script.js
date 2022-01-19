52962cd3eeb8bddea30589c440402fa7ec310459
jiggajigga <74821890+jiggajigga@users.noreply.github.com>
2022-01-19 16:16:10 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -6,15 +6,6 @@ paypal
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          items: [
-            {
-              id: 1,
-              quantity: 2,
-            },
-            { id: 2, quantity: 3 },
-          ],
-        }),
       })
         .then(res => {
           if (res.ok) return res.json()
