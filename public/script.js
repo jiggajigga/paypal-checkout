d9f51bc56b29fb0d1aa120355f24dc86b7d9b3cb
jiggajigga <74821890+jiggajigga@users.noreply.github.com>
2022-01-19 19:34:57 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -18,12 +18,21 @@ paypal
           console.error(e.error)
         })
     },
-    onApprove: function (data, actions) {
-      return actions.order.capture().then(function(orderData) {
+  // Finalize the transaction after payer approval
+        onApprove: function(data, actions) {
+          return actions.order.capture().then(function(orderData) {
             // Successful capture! For dev/demo purposes:
                 console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
                 var transaction = orderData.purchase_units[0].payments.captures[0];
                 alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+
+            // When ready to go live, remove the alert and show a success message within this page. For example:
+            // var element = document.getElementById('paypal-button-container');
+            // element.innerHTML = '';
+            // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+            // Or go to another URL:  actions.redirect('thank_you.html');
+          });
     },
   })
   .render("#paypal")
