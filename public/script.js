9bbd8b6d5d01637d756af2d367dbe5ad3ec352db
jiggajigga <74821890+jiggajigga@users.noreply.github.com>
2022-01-19 19:29:40 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -19,7 +19,11 @@ paypal
         })
     },
     onApprove: function (data, actions) {
-      return actions.order.capture()
+      return actions.order.capture().then(function(orderData) {
+            // Successful capture! For dev/demo purposes:
+                console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                var transaction = orderData.purchase_units[0].payments.captures[0];
+                alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
     },
   })
   .render("#paypal")
