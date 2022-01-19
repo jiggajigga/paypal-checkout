8e652020dacd0edb027504b6aa92232be5a2ea9a
jiggajigga <74821890+jiggajigga@users.noreply.github.com>
2022-01-19 16:12:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -31,37 +31,20 @@ app.get("/", (req, res) => {
 
 app.post("/create-order", async (req, res) => {
   const request = new paypal.orders.OrdersCreateRequest()
-  const total = req.body.items.reduce((sum, item) => {
-    return sum + storeItems.get(item.id).price * item.quantity
-  }, 0)
+ // const total = req.body.items.reduce((sum, item) => {
+  //  return sum + storeItems.get(item.id).price * item.quantity
+ // }, 0)
   request.prefer("return=representation")
   request.requestBody({
-    intent: "CAPTURE",
-    purchase_units: purchase_units: [
-      {
-        amount: {
-          currency_code: "USD",
-          value: total,
-          breakdown: {
-            item_total: {
-              currency_code: "USD",
-              value: total,
-            },
-          },
-        },
-        items: req.body.items.map(item => {
-          const storeItem = storeItems.get(item.id)
-          return {
-            name: storeItem.name,
-            unit_amount: {
-              currency_code: "USD",
-              value: storeItem.price,
-            },
-            quantity: item.quantity,
-          }
-        }),
-      },
-    ],
+   "intent": "CAPTURE",
+    "purchase_units": [
+    {
+      "amount": {
+        "currency_code": "AUD",
+        "value": "100.00"
+      }
+    }
+  ],
   })
 
   try {
